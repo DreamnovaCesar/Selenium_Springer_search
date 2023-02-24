31f0d19177b6ca0b9cf823c56627a738eeb742b5
Cesar Eduardo Muñoz Chavez <cesareduardomucha@hotmail.com>
2023-02-23 19:47:41 -0600

    SOLID mod 1

--- app/Test_/test.py
+++
@@ -1,5 +0,0 @@
-# * A list in Python that contains the names of columns for a dataframe.
-Columns_springer_info = ['Href', 'Title', 'Subtitle', 'Authors', 'Publication_title', 'Year', 'DOI']
-Columns_springer_info_lower = [i.lower() for i in Columns_springer_info]
-
-print(Columns_springer_info_lower)
