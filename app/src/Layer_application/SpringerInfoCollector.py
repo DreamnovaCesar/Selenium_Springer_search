10622c52978b03a6359cb4a33ec95178f9f52ea5
Cesar Eduardo Muñoz Chavez <cesareduardomucha@hotmail.com>
2023-03-19 22:52:25 -0600

    requirements 1.0

--- app/src/Layer_application/SpringerInfoCollector.py
+++ app/src/Layer_application/SpringerInfoCollector.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from ..Utilities import Settings
+from ..utilities import Settings
 
 
 class SpringerInfoCollector(InfoCollector):
